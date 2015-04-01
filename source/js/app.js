d6cce3b90a7cfa275561c516034113e8b93546a0
Tarqwyn <steven@tarquindandy.com>
2015-04-01 19:22:07 +0100

    basic nation filter emits and reacts to state events

--- source/js/app.js
+++ source/js/app.js
@@ -1,3 +1,12 @@
-define(['lib/news_special/bootstrap', 'lib/news_special/share_tools/controller'], function (news, ShareTools) {
+define(['lib/news_special/bootstrap', 'nation-filter', 'lib/news_special/share_tools/controller'], function (news, NationFilter, ShareTools) {
+    
+	addNationFilterToView();
     news.sendMessageToremoveLoadingImage();
+
+    function addNationFilterToView() {
+   		new NationFilter({
+   			'state': 'uk'
+   		});
+    }
+
 });
