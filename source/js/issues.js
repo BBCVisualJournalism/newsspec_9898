a04fcf2952a9668f3330ff864595cd20f525acb5
fewstera <aidan.fewster@bbc.co.uk>
2015-04-02 16:04:57 +0100

    Fixed button hover states on mobile devices

--- source/js/issues.js
+++ source/js/issues.js
@@ -8,7 +8,9 @@ define([
 		this.submitButton = $('.nav-buttons #choose-policy');
 
 		/* LISTENERS */
-		this.issues.on('click', $.proxy(this.issueClicked, this));
+		this.issues.on('click', this.issueClicked);
+		this.issues.on('mouseenter', this.issueHoverStart);
+		this.issues.on('mouseleave', this.issueHoverEnd);
 		this.submitButton.on('click', $.proxy(this.submit, this));
 	}
 
@@ -17,10 +19,23 @@ define([
 		issueClicked: function (event) {
 			var issueElm = $(event.currentTarget);
 			issueElm.toggleClass('issue__checked');
+			setTimeout(function () {
+				issueElm.removeClass('issue__hover');
+			}, 20);
+		},
+
+		issueHoverStart: function () {
+			var issueElm = $(event.currentTarget);
+			issueElm.addClass('issue__hover');
+		},
+
+		issueHoverEnd: function () {
+			var issueElm = $(event.currentTarget);
+			issueElm.removeClass('issue__hover');
 		},
 
 		submit: function () {
-			var selectedOptions = ['test'];
+			var selectedOptions = [];
 			this.issues.filter('.issue__checked').each(function () {
 				selectedOptions.push($(this).data('issue'));
 			});
